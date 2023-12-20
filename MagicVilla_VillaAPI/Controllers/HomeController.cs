11a0544dda52ac258b809b980627bc02920eeb5e
ythakur <thakuryash@live.com>
2023-12-20 18:28:55 -0500

    Crud funtions done

--- MagicVilla_VillaAPI/Controllers/HomeController.cs
+++ MagicVilla_VillaAPI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
@@ -8,14 +9,91 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : Controller
     {
         [HttpGet]
-       public IEnumerable<Villa> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+       public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<Villa>
-            {
-                new Villa{Id=1, Name="Pool View"},
-                new Villa{Id=2,Name="Beach View"}
-            };
+            return Ok(VillaStore.villaList);
 
         }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        /*      [ProducesResponseType(200, Type = typeof(VillaDTO))]
+                [ProducesResponseType(400)]
+                [ProducesResponseType(404)]*/
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        {
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(x => x.Id).FirstOrDefault().Id +1;
+            VillaStore.villaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla",new {id = villaDTO.Id}, villaDTO);
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+
+        public IActionResult DeleteVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
+
+        [HttpPut("{id: int}", Name = "UpdateVilla")]
+        public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+        {
+            if(id == null || id != villaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            villa.Name = villaDTO.Name;
+            villa.Occupancy = villaDTO.Occupancy;
+            villa.Sqft = villaDTO.Sqft;
+            return NoContent();
+
+        }
+
     }
 }
---
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int Sqft {  get; set; }
+
+        public Boolean Occupancy { get; set; }
+    }
+}
